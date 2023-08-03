4cd1bc50b336a29cdeb800bd2d2118266cadf4fe
Janaledro <alejandro.garfide@ciencias.unam.mx>
2023-08-03 16:24:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ st.header('Pepepepepeperrea')
 st.text('conshesumadre')
 st.text('sákalabailar')
 st.text('dile al diyei que no pare')
+
+import pandas as pd
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+stream.dataframe(my_fruit_list)
