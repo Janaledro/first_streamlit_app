4f126d1acd9bd46d7a58239b196661e8ad18e46e
Janaledro <alejandro.garfide@ciencias.unam.mx>
2023-08-14 11:20:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
-ruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered ', fruit_choice)
 
 import requests
